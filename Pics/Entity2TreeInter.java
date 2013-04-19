860bc4b7404068602ac15c8be6e8b78b23b3d2ec
hoodlake <zhangsj@yumuxiaoxiao.com>
2013-04-19 08:48:42 +0800

    msg

---
+++ Pics/Entity2TreeInter.java
@@ -0,0 +1,10 @@
+﻿//指定实现该接口的类的那些字段作为相应的返回字段
+public interface Entity2TreeInter {
+	public String getId();
+	public String getFatherId();
+	public String getText();
+	public boolean getExpanded();
+	public String getCls();
+	public boolean getLeaf();
+	//public boolean getChecked();
+}
